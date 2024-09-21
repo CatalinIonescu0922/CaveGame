b1810dd7f3f291df280c080b8bd0f05fd961ba67
Avram Traian <traian.avram1@gmail.com>
2024-09-21 22:06:11 +0300

    Engine+Game: Fixed copyright header name order

--- Engine/Source/Engine/Engine.h
+++ Engine/Source/Engine/Engine.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) Bengulescu Robert 2024. All rights reserved.
  * Copyright (c) Catalin Ionescu 2024. All rights reserved.
+ * Copyright (c) Robert Bengulescu 2024. All rights reserved.
  * Copyright (c) Traian Avram 2024. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0.
