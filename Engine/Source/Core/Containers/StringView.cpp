5d45e12ed81f978620c5863cebb39d7e94d4d25f
Avram Traian <traian.avram1@gmail.com>
2024-09-30 14:25:49 +0300

    Engine: Implemented the `StringView` container

---
+++ Engine/Source/Core/Containers/StringView.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) Catalin Ionescu 2024. All rights reserved.
+ * Copyright (c) Robert Bengulescu 2024. All rights reserved.
+ * Copyright (c) Traian Avram 2024. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#include <Core/Assertion.h>
+#include <Core/Containers/StringView.h>
+
+namespace CaveGame
+{
+
+StringView StringView::create_from_utf8(const char* characters, usize byte_count)
+{
+    StringView view;
+    view.m_characters = characters;
+    view.m_byte_count = byte_count;
+    return view;
+}
+
+StringView StringView::create_from_utf8(const char* null_terminated_characters)
+{
+    CAVE_ASSERT(null_terminated_characters != nullptr);
+    const char* characters = null_terminated_characters;
+    while (*characters)
+        ++characters;
+
+    StringView view;
+    view.m_characters = null_terminated_characters;
+    view.m_byte_count = (characters - null_terminated_characters);
+    return view;
+}
+
+} // namespace CaveGame
---
+++ Engine/Source/Core/Containers/StringView.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) Catalin Ionescu 2024. All rights reserved.
+ * Copyright (c) Robert Bengulescu 2024. All rights reserved.
+ * Copyright (c) Traian Avram 2024. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#pragma once
+
+#include <Core/CoreTypes.h>
+
+namespace CaveGame
+{
+
+#if CAVE_COMPILER_MSVC
+    // Disables the following compiler warning:
+    // 'operator ""sv': literal suffix identifiers that do not start with an underscore are reserved.
+    #pragma warning(push)
+    #pragma warning(disable : 4455)
+#endif // CAVE_COMPILER_MSVC
+
+//
+// A view towards a UTF-8 encoded string.
+// The held string is not null-terminated and can't be mutated by the string view.
+//
+class StringView
+{
+    friend class StringView constexpr operator""sv(const char*, usize);
+
+public:
+    NODISCARD static StringView create_from_utf8(const char* characters, usize byte_count);
+    NODISCARD static StringView create_from_utf8(const char* null_terminated_characters);
+
+public:
+    ALWAYS_INLINE constexpr StringView()
+        : m_characters(nullptr)
+        , m_byte_count(0)
+    {}
+
+    ALWAYS_INLINE constexpr StringView(const StringView& other)
+        : m_characters(other.m_characters)
+        , m_byte_count(other.m_byte_count)
+    {}
+
+    ALWAYS_INLINE constexpr StringView(StringView&& other) noexcept
+        : m_characters(other.m_characters)
+        , m_byte_count(other.m_byte_count)
+    {
+        other.m_characters = nullptr;
+        other.m_byte_count = 0;
+    }
+
+    ALWAYS_INLINE constexpr StringView& operator=(const StringView& other)
+    {
+        m_characters = other.m_characters;
+        m_byte_count = other.m_byte_count;
+        return *this;
+    }
+
+    ALWAYS_INLINE constexpr StringView& operator=(StringView&& other) noexcept
+    {
+        // Handle self-assignment case.
+        if (this == &other)
+            return *this;
+
+        m_characters = other.m_characters;
+        m_byte_count = other.m_byte_count;
+        other.m_characters = nullptr;
+        other.m_byte_count = 0;
+        return *this;
+    }
+
+public:
+    NODISCARD ALWAYS_INLINE const char* characters() const { return m_characters; }
+    NODISCARD ALWAYS_INLINE usize byte_count() const { return m_byte_count; }
+
+    NODISCARD ALWAYS_INLINE bool is_empty() const { return (m_byte_count == 0); }
+
+private:
+    const char* m_characters;
+    usize m_byte_count;
+};
+
+NODISCARD ALWAYS_INLINE constexpr StringView operator""sv(const char* ascii_literal, usize ascii_literal_character_count)
+{
+    // Because ASCII is a subset of UTF-8, no conversion is needed.
+    StringView view;
+    view.m_characters = ascii_literal;
+    view.m_byte_count = ascii_literal_character_count;
+    return view;
+}
+
+#if CAVE_COMPILER_MSVC
+    #pragma warning(pop)
+#endif // CAVE_COMPILER_MSVC
+
+} // namespace CaveGame
