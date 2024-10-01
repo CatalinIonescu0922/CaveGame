b2c7fc8e6e5795013e328f96142c893bcc825b15
Avram Traian <traian.avram1@gmail.com>
2024-10-01 15:40:06 +0300

    Meta: Renamed `premake5.lua` to `PremakeConfig.lua` to match the style

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -1,7 +1,7 @@
 @echo off
 pushd "%~dp0"
 
-call "Binaries/ThirdParty/Premake/premake5.exe" --file="premake5.lua" vs2022
+call "Binaries/ThirdParty/Premake/premake5.exe" --file="PremakeConfig.lua" vs2022
 
 popd
 pause
