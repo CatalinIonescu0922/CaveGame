765d093477093167b18b464d247d60cfd17ea778
Avram Traian <traian.avram1@gmail.com>
2024-10-02 14:23:52 +0300

    Engine: Added an include for the placement-new operator (among others)

---
+++ Engine/Source/Core/Memory/New.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Catalin Ionescu 2024. All rights reserved.
+ * Copyright (c) Robert Bengulescu 2024. All rights reserved.
+ * Copyright (c) Traian Avram 2024. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#pragma once
+
+#include <new>
